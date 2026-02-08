bf5a0d117226f7cba5fd6df8f474486e45230d8b
PRATHAM MEHTA <82135692+Sontakkepratham@users.noreply.github.com>
2026-02-08 20:45:58 +0530

    Add AI Tourist App with user input and predictions
    Implement AI Tourism Recommendation System using Streamlit.

---
+++ app.py
@@ -0,0 +1,52 @@
+import streamlit as st
+import pandas as pd
+import pickle
+
+st.set_page_config(page_title="AI Tourist App", layout="wide")
+
+st.title("AI Tourism Recommendation System")
+
+def load_data():
+  df = pickle.load(open("data.pkl","rb"))
+  return df
+
+def load_model():
+  clf = pickle.load(open("clf_model.pkl", "rb"))
+  return clf
+
+df = load_data()
+clf = load_model()
+
+st.sidebar.header("User Input")
+
+user_id = st.sidebar.selectbox(
+  "Select User ID",
+  df["UserId"].unique()
+)
+
+st.subheader("Predicted Visit Mode")
+
+features = [
+  "VisitYear",
+  "VisitMonth",
+  "attraction_popularity",
+  "attr_avg_rating",
+  "user_total_visits",
+  "user_avg_rating"
+]
+
+user_data = df[df["UserId] == user_id][features].iloc[0]
+
+prediction = clf.predict([user_data])[0]
+
+st.success(f"Predicted Visit Mode (Encoded): {prediction}")
+
+st.subheader("User History")
+
+st.dataframe(
+  df[df["UserId"] == user_id][[
+      "AttractionId",
+      "Rating",
+      "AttractionType"
+  ]].head(10)
+)
